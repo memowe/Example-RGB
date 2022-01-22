c1aba3ddc55f636da7bcf880ebf5cc841d744e6d
Mirko Westermeier <mirko@westermeier.de>
2022-01-22 19:08:30 +0100

    cleanup unitTests name

--- test/tests.hs
+++ test/tests.hs
@@ -3,9 +3,9 @@ import Test.Tasty ( defaultMain, testGroup )
 import RGB.TestRGB ( testBasics )
 import RGB.TestCodewars ( testCodewars )
 
-unitTests = testGroup "RGB tests"
+tests = testGroup "RGB tests"
   [ testBasics
   , testCodewars
   ]
 
-main = defaultMain $ testGroup "Tests" [unitTests]
+main = defaultMain $ testGroup "Tests" [tests]
